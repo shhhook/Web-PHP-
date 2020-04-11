29b1ae9f1fe07e3805e20df16ebc85bd50245208
shhhook <shhhook04@gmail.com>
2020-04-11 12:26:59 +0300

    init labs 1-3

---
+++ migrations/m200213_184142_create_category_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%category}}`.
+ */
+class m200213_184142_create_category_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('category', [
+            'id' => $this->primaryKey(),
+            'title'=>$this->string(),
+        ]);
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%category}}');
+    }
+}
---
+++ migrations/m200213_184154_create_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%user}}`.
+ */
+class m200213_184154_create_user_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('user', [
+            'id' => $this->primaryKey(),
+            'name'=>$this->string(),
+            'email'=>$this->string()->defaultValue(null),
+            'password'=>$this->string(),
+            'isAdmin'=>$this->string()->defaultValue(0),
+            'photo'=>$this->string()->defaultValue(null),
+        ]);
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%user}}');
+    }
+}
